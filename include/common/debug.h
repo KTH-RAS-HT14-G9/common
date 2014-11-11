eff37b729a6cbbf94fd22da6861b9ff0db641909
Max Losch <mmlosch@kth.se>
2014-11-11 17:04:32 +0100

    global debug definitions

---
+++ include/common/debug.h
@@ -0,0 +1,13 @@
+#ifndef COMMON_DEBUG_H
+#define COMMON_DEBUG_H
+
+//------------------------------------------------------------------------------
+// Vision
+#define ENABLE_VISUALIZATION_PLANES 0
+#define ENABLE_VISUALIZATION_ROIS 0
+
+//------------------------------------------------------------------------------
+// Time profiling
+#define ENABLE_TIME_PROFILING 0
+
+#endif // COMMON_DEBUG_H
