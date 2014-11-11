3c7ed911348aa9828fc878ba34519dd01e1a4f50
Max Losch <mmlosch@kth.se>
2014-11-11 17:04:19 +0100

    default color set class

--- include/common/types.h
+++ include/common/types.h
@@ -18,7 +18,56 @@ namespace common {
     public:
         Color(int cr, int cg, int cb)
             :r(cr),g(cg),b(cb) {}
+
+        Color(int rgb) {
+            r = (rgb >> 16) & 0x0000ff;
+            g = (rgb >> 8)  & 0x0000ff;
+            b = (rgb)       & 0x0000ff;
+        }
+
         int r, g, b;
+    };
+
+    class Colors {
+    public:
+        Colors() {
+            _colors.push_back(Color(255,0,0));
+            _colors.push_back(Color(0,255,0));
+            _colors.push_back(Color(0,0,255));
+            _colors.push_back(Color(255,255,0));
+            _colors.push_back(Color(0,255,255));
+        }
+
+        Color next() {
+            if (_unusedIdx >= _colors.size()) {
+                return Color(rand());
+            }
+            else {
+                return _colors[_unusedIdx++];
+            }
+        }
+
+        void reset() {
+            _unusedIdx = 0;
+        }
+
+    protected:
+        std::vector<Color> _colors;
+        int _unusedIdx;
+    };
+
+    class Timer {
+    public:
+        void start() {
+            _start = ros::Time::now();
+        }
+
+        double elapsed() {
+            return ros::Time::now().toSec()-_start.toSec();
+        }
+
+    protected:
+        ros::Time _start;
     };
 
 }
