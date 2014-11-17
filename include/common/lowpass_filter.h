a68764e183891d0518691025b6aebd5c7dfc3b2e
Max Losch <mmlosch@kth.se>
2014-11-17 10:24:06 +0100

    Add lowpass filter and hystereis class

---
+++ include/common/lowpass_filter.h
@@ -0,0 +1,38 @@
+#ifndef LOWPASS_FILTER_H
+#define LOWPASS_FILTER_H
+
+
+namespace common {
+
+    class LowPassFilter {
+    public:
+        LowPassFilter(double inertia) 
+        :_inertia(inertia)
+        ,_initialized(false)
+        {}
+
+        double filter(double measurement) {
+            if (_initialized == false) {
+                _last_y = measurement;
+                _initialized = true;
+            }
+
+            double y = (1.0-_inertia)*measurement + _inertia*_last_y;
+            _last_y = y;
+
+            return y;
+        }
+
+        void set_inertia(double inertia) {
+            _inertia = inertia;
+        }
+
+    protected:
+        double _inertia;
+        double _last_y;
+        bool _initialized;
+    };
+
+}
+
+#endif // COMMON_OBB_H
--- include/common/util.h
+++ include/common/util.h
@@ -13,6 +13,61 @@ namespace common {
         return value;
     }
 
+    template<typename T>
+    class Hysteresis {
+    public:
+        Hysteresis(T lower_threshold,
+                   T upper_threshold,
+                   T upper_output,
+                   T lower_output,
+                   T initial_state,
+                   T initial_output)
+
+            :_lower_threshold(lower_threshold)
+            ,_upper_threshold(upper_threshold)
+            ,_upper_output(upper_output)
+            ,_lower_output(lower_output)
+            ,_last_state(initial_state)
+            ,_last_output(initial_output)
+        {
+        }
+
+        T apply(T state)
+        {
+            T output;
+
+            if (state > _last_state)
+            {
+                if (state > _upper_threshold)
+                    output = _upper_output;
+                else
+                    output = _last_output;
+            }
+            else
+            {
+                if (state < _lower_threshold)
+                    output = _lower_output;
+                else
+                    output = _last_output;
+            }
+
+            _last_output = output;
+            _last_state = state;
+
+            return output;
+        }
+
+    protected:
+        T _last_state;
+        T _last_output;
+
+        T _lower_threshold;
+        T _upper_threshold;
+
+        T _lower_output;
+        T _upper_output;
+    };
+
 }
 
 #endif // COMMON_UTIL_H
