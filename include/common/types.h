8e44ec30aba8dbb395f8b6a11d58ac2535d43193
Max Losch <mmlosch@kth.se>
2014-11-23 16:41:42 +0100

    types for color and object recognition

--- include/common/types.h
+++ include/common/types.h
@@ -13,6 +13,76 @@ namespace common {
     typedef pcl::PointCloud<pcl::PointXYZRGB> PointCloudRGB;
     typedef PointCloudRGB::ConstPtr SharedPointCloudRGB;
 
+    class NameAndProbability
+    {
+    public:
+        NameAndProbability()
+            :_name("undefined")
+            ,_probability(std::numeric_limits<double>::quiet_NaN())
+        {
+        }
+
+        NameAndProbability(const std::string& nam, double prob)
+            :_name(nam)
+            ,_probability(prob)
+        {}
+
+        bool is_undefined() const {
+            return std::isnan(_probability);
+        }
+
+        const std::string& name() const {return _name;}
+        const double probability() const {return _probability;}
+
+    protected:
+        std::string _name;
+        double _probability;
+    };
+
+    class ObjectColorMap {
+    public:
+        static ObjectColorMap& instance() {
+            static ObjectColorMap instance;
+            return instance;
+        }
+
+        double get(const std::string& name) {
+            return _colormap.at(name);
+        }
+
+        const std::vector<std::string>& names() {
+            return _names;
+        }
+
+    private:
+
+        std::vector<std::string> _names;
+
+        typedef std::pair<std::string, double> NameAndColor;
+        std::map<std::string, double> _colormap;
+
+        ObjectColorMap() {
+            make("red",361.30828);
+            make("yellow",34.643364);
+            make("blue",215.820892);
+            make("green",110.0);
+            make("green_light",65.0);
+            make("orange",20.0);
+            make("plurple",280.0);
+        }
+
+        void make(const std::string& name, double value) {
+            NameAndColor nc;
+            nc.first = name;
+            nc.second = value;
+
+            _colormap.insert(nc);
+            _names.push_back(name);
+        }
+
+        ObjectColorMap(ObjectColorMap const&);
+        void operator=(ObjectColorMap const&);
+    };
 
     class Color {
     public:
